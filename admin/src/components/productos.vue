1561a11dcabda9c6d8922b789fa7de3b058cf692
themasterdeveloper <omar.melendrez@am.sony.com>
2017-09-27 12:12:14 -0300

    Create products table

--- admin/src/components/productos.vue
+++ admin/src/components/productos.vue
@@ -4,6 +4,44 @@
       <h1 class="md-title">Administrar Productos</h1>
       <router-link tag="md-button" :to="{ name: 'Home' }" class="md-raised md-default">Home</router-link>
     </md-toolbar>
+
+    <md-table-card>
+      <md-toolbar>
+        <h1 class="md-title">Productos</h1>
+        <md-button class="md-icon-button">
+          <md-icon>filter_list</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button">
+          <md-icon>search</md-icon>
+        </md-button>
+      </md-toolbar>
+
+      <md-table md-sort="code" md-sort-type="asc" @select="onSelect" @sort="onSort">
+        <md-table-header>
+          <md-table-row>
+            <md-table-head md-sort-by="code">Código</md-table-head>
+            <md-table-head md-sort-by="name">Nombre</md-table-head>
+            <md-table-head md-numeric>Precio</md-table-head>
+            <md-table-head>Categoría</md-table-head>
+            <md-table-head>Status</md-table-head>
+            <md-table-head></md-table-head>
+          </md-table-row>
+        </md-table-header>
+
+        <md-table-body>
+          <md-table-row v-for="(row, rowIndex) in products" :key="rowIndex" :md-item="row" md-auto-select md-selection>
+            <md-table-cell>{{row.code}}</md-table-cell>
+            <md-table-cell>{{row.name}}</md-table-cell>
+            <md-table-cell md-numeric>{{row.price}}</md-table-cell>
+            <md-table-cell>{{row.category_id}}</md-table-cell>
+            <md-table-cell>{{row.status}}</md-table-cell>
+          </md-table-row>
+        </md-table-body>
+      </md-table>
+
+      <md-table-pagination md-size="5" md-total="10" md-page="1" md-label="Rows" md-separator="of" :md-page-options="[5, 10, 25, 50]" @pagination="onPagination"></md-table-pagination>
+    </md-table-card>
   </div>
 </template>
 
@@ -19,7 +57,7 @@ export default {
     fetchProducts() {
       this.$http.get('http://localhost:3000/products')
         .then((res) => {
-          console.log(res.body);
+          this.products = res.body;
         })
         .catch((err) => {
           console.log(err.data);
