2de54a1f65b69bbc3fdb133b43158ec21ab15915
themasterdeveloper <omar.melendrez@am.sony.com>
2017-09-27 11:34:26 -0300

    Add base __dirname to router

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -4,28 +4,29 @@ import Resource from 'vue-resource';
 
 import Home from '@/components/home';
 import Categorias from '@/components/categorias';
-import Productos from '@/components/productos';
+import Products from '@/components/productos';
 
 Vue.use(Router);
 Vue.use(Resource);
 
 export default new Router({
   mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home,
+      component: Home
     },
     {
       path: '/categorias',
       name: 'Categorias',
-      component: Categorias,
+      component: Categorias
     },
     {
       path: '/productos',
       name: 'Productos',
-      component: Productos,
-    },
-  ],
+      component: Products
+    }
+  ]
 });
