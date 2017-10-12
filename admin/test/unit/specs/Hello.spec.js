2a6d9af94dfd3da5e1bd7ab40c2860e6cce0923b
themasterdeveloper <omar.melendrez@am.sony.com>
2017-10-12 11:01:15 -0300

    Define first unit test

--- admin/test/unit/specs/Hello.spec.js
+++ admin/test/unit/specs/Hello.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import Hello from '@/components/Hello';
+import categories from '@/components/categories';
 
-describe('Hello.vue', () => {
+describe('categories.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello);
+    const Constructor = Vue.extend(categories);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    expect(vm.$el.querySelector('h1.md-title').textContent)
       .to.equal('Welcome to Your Vue.js App');
   });
 });
