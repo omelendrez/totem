8a94fd75e56e79768e55ecb0c16ddd6a81aac76a
omelendrez <omar.melendrez@gmail.com>
2018-10-30 03:01:38 -0300

    Improve cart and products list

--- client/src/components/cart.vue
+++ client/src/components/cart.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="cart">
+    <h1>Carrito</h1>
+    <md-chip v-if="totalBasket" class="md-accent cart-total">
+      $ {{totalBasket}}
+      <md-icon class="cart-icon pull-right">shopping_cart</md-icon>
+      Pagar
+    </md-chip>
+    <h4 v-else>No tenés items en tu carrito</h4>
+
     <md-list class="md-triple-line">
-      <md-list-item v-for="basketItem in basketItems" :key="basketItem.id">
+      <md-list-item v-for="(basketItem, index) in basketItems" :key="index">
         <md-avatar>
           <md-image :md-src="basketItem.image"></md-image>
         </md-avatar>
         <div class="md-list-text-container">
-          <span>{{basketItem.name}}</span>
+          <span><strong>{{basketItem.name}}</strong></span>
           <span>{{basketItem.category.name}}</span>
-          <p>${{basketItem.price}}</p>
+          <p><span class="price">${{basketItem.price}}</span></p>
         </div>
         <md-button class="md-icon-button md-raised md-accent" v-on:click.native="eliminar(basketItem)">
           <md-icon>clear</md-icon>
@@ -29,6 +37,9 @@ export default {
   computed: {
     basketItems() {
       return store.getters.basket;
+    },
+    totalBasket() {
+      return store.getters.totalBasket;
     }
   },
   methods: {
@@ -57,10 +68,14 @@ export default {
   padding: 4px;
   overflow: auto;
   width: 100%;
+  text-align: center;
   border-left: 1px solid lavender;
 }
 .md-icon-button i {
   color: #fff;
+}
+.md-image {
+  height: auto;
 }
 ::-webkit-scrollbar {
   width: 0px;
@@ -74,7 +89,7 @@ table tbody tr td {
   color: #fff;
 }
 .cart-total {
-  height: 60px;
+  height: 64px;
   padding: 24px;
   font-size: 2em;
   font-weight: bold;
@@ -84,4 +99,9 @@ table tbody tr td {
 .md-list-text-container span {
   font-size: small;
 }
+.price {
+  float: right;
+  font-weight: bold;
+}
+
 </style>
--- client/src/components/products.vue
+++ client/src/components/products.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="products">
+    <h1>Productos</h1>
+    <h3>{{selectedCategoryName}}</h3>
     <md-layout id="container">
-      <md-layout md-align="center" v-on:click.native="openItem(product)" md-flex-large="33" v-for="product in products" :key="product.id" :md-item="product">
+      <md-layout md-align="center" md-flex-large="33" v-for="product in products" :key="product.id" :md-item="product">
         <div class="product">
           <md-image :md-src="product.image"></md-image>
           <div class="md-title">
-            {{product.name}}
+            <strong>{{product.name}}</strong>
           </div>
           <div class="md-subhead">
             {{product.price}}
           </div>
+          <md-button class="md-fab md-primary md-mini left" v-on:click.native="openItem(product)">
+            <md-icon>help_outline</md-icon>
+          </md-button>
+          <md-button class="md-fab md-plain md-mini right" v-on:click.native="buyItem(product)">
+            <md-icon>add</md-icon>
+          </md-button>
         </div>
       </md-layout>
     </md-layout>
@@ -33,6 +41,9 @@ export default {
     },
     itemSet() {
       return store.getters.itemSet;
+    },
+    selectedCategoryName() {
+      return store.getters.selectedCategory ? store.getters.selectedCategory.name : 'TODAS LAS CATEGORÍAS';
     }
   },
   created() {
@@ -42,7 +53,13 @@ export default {
     openItem(item) {
       item.from = "products";
       store.dispatch(types.SET_ITEM, item);
+    },
+    buyItem(item) {
+      item.from = "products";
+      store.dispatch(types.SET_ITEM, item);
+      store.dispatch(types.ADD_ITEM, item);
     }
+
   }
 };
 </script>
@@ -52,10 +69,16 @@ export default {
   overflow: auto;
   width: 100%;
   left: 0;
+  text-align: center;
+  padding: 0;
 }
 
 .product {
   text-align: center;
+  height: 220px;
+  min-width: 140px;
+  margin: 0;
+  padding: 0;
   margin-bottom: 40px;
 }
 
@@ -66,5 +89,11 @@ export default {
 
 ::-webkit-scrollbar {
   width: 0px;
+}
+.left {
+  float: left;
+}
+.right {
+  float: right;
 }
 </style>
