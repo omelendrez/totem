86df16af6a72da3cb1e7b20c5eca605c672ad70f
omelendrez <omar.melendrez@gmail.com>
2018-11-04 22:36:23 -0300

    First commit of new totem frontend

---
+++ totem/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container class="home">
+    <div class="categories">
+      <Categories />
+    </div>
+    <div class="products">
+      <Products />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import store from "@/store";
+import Categories from "@/components/Categories";
+import Products from "@/components/Products";
+export default {
+  name: "home",
+  store,
+  components: {
+    Categories,
+    Products
+  },
+  data() {
+    return {};
+  },
+  created() {
+    store.dispatch("loadBasket");
+  }
+};
+</script>
+<style scoped>
+.categories {
+  position: absolute;
+  left: 0;
+  width: 20%;
+}
+.products {
+  left: 20%;
+  position: absolute;
+  width: 60%;
+}
+.basket {
+  left: 80%;
+  position: absolute;
+  width: 20%;
+}
+</style>
