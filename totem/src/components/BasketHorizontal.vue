55174c749517eafbf4267a3d7c271b650bca300d
omelendrez <omar.melendrez@gmail.com>
2019-02-15 20:22:16 -0300

    Add CC payment component

--- totem/src/components/BasketHorizontal.vue
+++ totem/src/components/BasketHorizontal.vue
@@ -55,6 +55,7 @@
 import store from "@/store";
 import Checkout from "@/components/Checkout";
 import Confirm from "@/components/Confirm";
+import CCPayment from '@/components/CCPayment'
 import Processing from "@/components/Processing";
 export default {
   name: "Basket",
@@ -62,6 +63,7 @@ export default {
   components: {
     Checkout,
     Confirm,
+    CCPayment,
     Processing
   },
   props: {
---
+++ totem/src/components/CCPayment.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog" persistent max-width="600">
+      <v-card>
+        <v-card-title primary-title class="headline error white--text"></v-card-title>
+        <v-card-text class="headline"></v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn large @click="doConfirm(false)"></v-btn>
+          <v-btn large color="error" @click="doConfirm(true)"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "CCPayment",
+  data() {
+    return {};
+  }
+};
+</script>
