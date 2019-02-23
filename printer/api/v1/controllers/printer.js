05c211d598652b3c33f4afcfe630d80fbf13d61e
omelendrez <omar.melendrez@gmail.com>
2019-02-23 15:15:25 -0300

    test printer

--- printer/api/v1/controllers/printer.js
+++ printer/api/v1/controllers/printer.js
@@ -3,16 +3,16 @@ const ThermalPrinter = require('node-thermal-printer').printer
 const PrinterTypes = require('node-thermal-printer').types
 let printer = new ThermalPrinter({
   type: PrinterTypes.EPSON,
-  interface: '\\.COM1'
+  interface: "\\.\COM1"
 })
 module.exports = {
-  print(req, res) {
+  async print(req, res) {
     const order =req.body.orderData
     printer.alignCenter()
     printer.println(order.order_number)
     printer.cut()
     try {
-      let execute = printer.execute()
+      let execute = await printer.execute()
       console.error('Print done!', order.order_number, execute)
     } catch (error) {
       console.log('Print failed:', error)
