84588a5d6d4228d4529f9cbec79d3405c56e360f
omelendrez <omar.melendrez@gmail.com>
2019-02-24 23:56:12 -0300

    Printer setup

--- printer/api/v1/controllers/printer.js
+++ printer/api/v1/controllers/printer.js
@@ -3,21 +3,34 @@ const ThermalPrinter = require('node-thermal-printer').printer
 const PrinterTypes = require('node-thermal-printer').types
 let printer = new ThermalPrinter({
   type: PrinterTypes.EPSON,
-  interface: `\\.\COM1`
+  interface: 'tcp://192.168.0.5'
 })
 module.exports = {
   async print(req, res) {
     const order = req.body.orderData
-    console.log(`Printing order ${order.order_number}`)
     const isConnected = await printer.isPrinterConnected()
     if (!isConnected) {
-      res
-        .status(404)
-        .send({ error: true, message: 'Atención!!! Impresora de tickets desconectada' })
+      res.status(404).send({
+        error: true,
+        message: 'Atención!!! Impresora de tickets desconectada'
+      })
       return
     }
     printer.alignCenter()
+
     printer.println(order.order_number)
+    var data = order.order_number // Barcode data (string or buffer)
+    var type = 74 // Barcode type (See Reference)
+    var settings = {
+      // Optional Settings
+      hriPos: 0, // Human readable character 0 - 3 (none, top, bottom, both)
+      hriFont: 0, // Human readable character font
+      width: 3, // Barcode width
+      height: 168 // Barcode height
+    }
+
+    printer.printBarcode(data, type, settings)
+
     printer.cut()
     printer.execute(err => {
       if (err) {
