499bc3afb6b468d7b0e819ce10cf2a9cb326ed5c
omelendrez <omar.melendrez@gmail.com>
2019-03-09 11:19:59 -0300

    Refactor print server

--- totem/server.js
+++ totem/server.js
@@ -1,7 +1,26 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const app = express()
+
+app.use(serveStatic(__dirname + '/dist'))
+
+const port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port) // eslint-disable-line no-console
+
+/*
+Antoher way
 const express = require('express');
-const serveStatic = require('serve-static');
+const path = require('path');
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);  // eslint-disable-line no-console
+console.log('Server started '+ port);  // eslint-disable-line no-console
+*/
