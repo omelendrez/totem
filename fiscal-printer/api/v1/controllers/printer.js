cebdaa2a44dc120a1605ae5702a3d08772a5ff36
unknown <omar.melendrez@gmail.com>
2019-08-01 10:46:46 -0300

    Implement new tests

--- fiscal-printer/api/v1/controllers/printer.js
+++ fiscal-printer/api/v1/controllers/printer.js
@@ -5,9 +5,9 @@ module.exports = {
   async test(req, res) {
     const ConsultarVersionDll = edge.func({
       source: "application.cs",
-      //assemblyFile: "EpsonFiscalInterface.dll",
-      //typeName: "EpsonFiscalInterface",
-      methodName: 'ConsultarVersionDll' // This must be Func<object,Task<object>>
+      //assemblyFile: "dlls/comEpsonFiscalDriver.ocx",
+      typeName: "comEpsonFiscalDriverLib.EpsonFiscalDriver",
+      methodName: 'ImprimirCierreX' // This must be Func<object,Task<object>>
     });
     ConsultarVersionDll("", function (err, result) {
       if (err) return res.status(500).json(err)
