754f9983e6695b359c243ed8f1b072d31b2b6c6d
unknown <omar.melendrez@gmail.com>
2019-08-03 17:39:17 -0300

    Fix totem update path

--- server/api/v1/routes/totem.js
+++ server/api/v1/routes/totem.js
@@ -6,8 +6,8 @@ const router = express.Router()
 router.get('/:id', totem.findById)
 router.get('/', totem.findAll)
 router.post('/', totem.create)
-router.put('/:id', totem.changeStatus)
 router.put('/activate/:id', totem.activate)
 router.put('/de-activate/:id', totem.deActivate)
+router.put('/:id', totem.changeStatus)
 
 module.exports = router
