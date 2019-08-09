bb4ce62c6d2d8068781478da2ce957e80f4a5ad8
omelendrez <omar.melendrez@gmail.com>
2019-08-09 20:12:44 -0300

    Remove unnecessary config data

--- fiscal-printer/api/v1/config/index.js
+++ fiscal-printer/api/v1/config/index.js
@@ -1,38 +1,4 @@
-module.exports.header = () => {
-  return [
-    'EXTALL S.A.',
-    'CUIT Nro.: 30-70835966-8',
-    'DONADO 28 BAHIA BLANCA',
-    'IVA RESPONSABLE INSCRIPTO',
-    'A CONSUMIDOR FINAL',
-    'P.V. Nro.: 0034'
-  ]
-}
-
 module.exports.defaultValues = {
   port: "1",
   speed: 9600
 }
-
-const printers = [
-  {
-    id: 1,
-    name: 'Totem thermal printer',
-    interface: 'tcp://192.168.100.85',
-    isFiscal: false
-  },
-  {
-    id: 2,
-    name: 'Command thermal printer',
-    interface: 'tcp://192.168.100.39',
-    isFiscal: false
-  },
-  {
-    id: 3,
-    name: 'Fiscal printer',
-    interface: '???',
-    isFiscal: true
-  }
-]
-
-module.exports.getPrinter = id => printers.find(printer => printer.id === id)
