bd630916a50cccd084289d5f6e7e0edea1f31c62
omelendrez <omar.melendrez@gmail.com>
2019-08-22 18:23:29 -0300

    Rename totem and add printer calls to turns

--- scanner/src/config/index.js
+++ scanner/src/config/index.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
-export const backendServer = 'http://TOTEM00:3000'
-const printServerURL = 'http://TOTEM00:3020'
-const fiscalPrinterURL = 'http://TOTEM00:3060'
+export const backendServer = 'http://totem00:3000'
+const printServerURL = 'http://totem00:3020'
+const fiscalPrinterURL = 'http://totem00:3060'
 
 const servers = {
   databaseServer: Axios.create({ baseURL: backendServer }),
