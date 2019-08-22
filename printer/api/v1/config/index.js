bad67b7b07d1b61cf34f68c55a908e8bacbd4a0b
omelendrez <omar.melendrez@gmail.com>
2019-08-22 14:39:41 -0300

    Update printer IPs

--- printer/api/v1/config/index.js
+++ printer/api/v1/config/index.js
@@ -13,13 +13,13 @@ const printers = [
   {
     id: 1,
     name: 'Totem thermal printer',
-    interface: 'tcp://192.168.100.85',
+    interface: 'tcp://192.168.20.198',
     isFiscal: false
   },
   {
     id: 2,
     name: 'Command thermal printer',
-    interface: 'tcp://192.168.100.39',
+    interface: 'tcp://192.168.20.199',
     isFiscal: false
   },
   {
