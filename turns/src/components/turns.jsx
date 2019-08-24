9aff219c819a6bd4d9226eb70be24c21e5301bb0
omelendrez <omar.melendrez@gmail.com>
2019-08-24 11:20:20 -0300

    Fix turns design

--- turns/src/components/turns.jsx
+++ turns/src/components/turns.jsx
@@ -3,9 +3,7 @@ import Turn from './turn'
 const Turns = ({ title, turns, icon }) => {
   return (
     <React.Fragment>
-      <h1>
-        <span className="badge sub-title">{title}</span>
-      </h1>
+      <div className="sub-title">{title}</div>
       {turns.map(turn => (
         <Turn key={turn.order_number} turn={turn.order_number} icon={icon} />
       ))}
