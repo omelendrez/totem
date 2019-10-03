ddad7052aea201aab77700a8611e8a317d49fd37
omelendrez <omar.melendrez@gmail.com>
2019-10-03 19:46:19 -0300

    Get cc-payment records

--- server/api/v1/routes/cc_payment.js
+++ server/api/v1/routes/cc_payment.js
@@ -3,5 +3,6 @@ const ccPayment = require("../controllers/cc_payment")
 const express = require("express")
 const router = express.Router()
 
+router.get("/", ccPayment.findAll)
 router.post("/", ccPayment.create)
 module.exports = router
