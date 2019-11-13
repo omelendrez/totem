f514c9a0a3dd98d4360f14982525bc9bb9b774c8
omelendrez <omar.melendrez@gmail.com>
2019-11-12 21:22:23 -0300

    Add drink confirmation

--- totem/src/components/Confirm.vue
+++ totem/src/components/Confirm.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="dialog" persistent max-width="600">
-      <v-card>
-        <v-card-title primary-title  class="headline error white--text">{{title}}</v-card-title>
-        <v-card-text class="headline">{{message}}</v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn large @click.stop="doConfirm(false)">{{buttonNoMsg}}</v-btn>
-          <v-btn large color="error" @click.stop="doConfirm(true)">{{buttonOkMsg}}</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+  <v-dialog v-model="dialog" persistent max-width="600">
+    <v-layout v-bind:class="{ confirm: image }">
+      <v-flex v-bind:class="{ padding: image }">
+        <v-card>
+          <v-img v-if="image" :src="image" contain></v-img>
+          <v-card-title v-if="!image" primary-title class="headline error white--text">{{title}}</v-card-title>
+          <v-card-text v-if="!image" class="headline">{{message}}</v-card-text>
+          <v-card-actions v-if="image">
+            <v-btn
+              large
+              absolute
+              bottom
+              left
+              color="error"
+              class="mb-3"
+              @click.stop="doConfirm(false)"
+            >
+              <v-icon>close</v-icon>&nbsp;cambiar
+            </v-btn>
+            <v-btn
+              large
+              absolute
+              bottom
+              right
+              color="primary"
+              class="mb-3"
+              @click.stop="doConfirm(true)"
+            >
+              <v-icon>shopping_cart</v-icon>&nbsp;agregar
+            </v-btn>
+          </v-card-actions>
+          <v-card-actions v-if="!image">
+            <v-spacer></v-spacer>
+            <v-btn large @click.stop="doConfirm(false)">{{buttonNoMsg}}</v-btn>
+            <v-btn large color="error" @click.stop="doConfirm(true)">{{buttonOkMsg}}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-dialog>
 </template>
 
 <script>
@@ -37,6 +64,10 @@ export default {
     confirm: {
       type: Function,
       default: undefined
+    },
+    image: {
+      type: String,
+      default: ""
     }
   },
   watch: {
@@ -57,5 +88,10 @@ export default {
 };
 </script>
 <style scoped>
+.confirm {
+  background: #ee3542;
+}
+.padding {
+  margin: 2rem;
+}
 </style>
-
